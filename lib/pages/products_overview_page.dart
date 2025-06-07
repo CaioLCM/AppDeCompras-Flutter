cba557dd0d01d9e90a6a8107e61eeef80502fb3e
Caio Lene Campos Magalhães <caio.lene.magalhaes@gmail.com>
2025-06-07 17:10:04 -0300

    Desenvolvi a opção de mostrar apenas os itens marcados como favoritos

--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,16 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:semana6_7/components/product_grid.dart';
-class ProductsOverviewPage extends StatelessWidget {
+import 'package:semana6_7/models/product_list.dart';
 
+enum FilterOptions {
+  Favorite,
+  All,
+}
+class ProductsOverviewPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<ProductList>(context);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).primaryColor,
-        title: Center(child: Text("Minha loja", style: TextStyle(color: Colors.white),)),
+        title: Center(
+          child: Text("Minha loja", style: TextStyle(color: Colors.white)),
+        ),
+        actions: [
+          PopupMenuButton(
+            itemBuilder:
+                (_) => [
+                  PopupMenuItem(value: FilterOptions.Favorite, child: Text("Somente favoritos")),
+                  PopupMenuItem(value: FilterOptions.All, child: Text("Todos")),
+                ],
+                onSelected: (FilterOptions selectedValue){
+                  if(selectedValue == FilterOptions.Favorite){
+                    provider.showFavoriteOnly();
+                  } else{
+                    provider.showAll();
+                  }
+                },
+          ),
+        ],
       ),
-      body: 
-      ProductGrid()
+      body: ProductGrid(),
     );
   }
 }
